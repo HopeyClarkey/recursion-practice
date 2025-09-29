8503415e13da30547d9c952fc8da5eec1ed21be2
Hope Clarke <thehopeclarke@gmail.com>
2025-09-29 16:39:25 -0500

    partial palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -139,17 +139,16 @@ var reverse = function(string, output = '') {
 }
 
 // 10. Write a function that determines if a string is a palindrome.
-var palindrome = function(string, output = '') {
+var palindrome = function(string) {
 //base case return if true to stop the loop
-if (string === ''){
+if (string.length <= 1){
   return true;
 }
-let newString = string.split('');
-if (newString.pop() !== newString.shift()){
+if (string[0] !== string[string.length - 1]){
   return false;
 }
 //recursion
-  return palindrome(newString);
+  return palindrome(string.slice(1,-1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
