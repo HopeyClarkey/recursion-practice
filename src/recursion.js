80b12c3b97148d056c0105695d4c5099153224a5
Hope Clarke <thehopeclarke@gmail.com>
2025-09-30 10:52:55 -0500

    some numToText

--- src/recursion.js
+++ src/recursion.js
@@ -388,7 +388,11 @@ var alternateSign = function(array) {
 // 35. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = function(str) {
+var numToText = function(str, output = '') {
+  var wrds = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+
+  return numToText(str.slice(1), output);
 };
 
 // *** EXTRA CREDIT ***
